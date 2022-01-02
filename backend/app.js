7eb051bbf56c7560f715e4e8638bfb0378b8a1a9
KevinC0210 <kevincavalar@gmx.de>
2022-01-02 13:30:30 +0100

    Merge bra

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const router = require("./router/router");
 const { v4: uuidv4 } = require("uuid");
 const app = express();
@@ -8,6 +10,8 @@ const port = 3030;
 
 const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
 const sessionSecret = uuidv4();
+
+app.use(bodyParser.json());
 
 app.use(
   session({
@@ -17,6 +21,13 @@ app.use(
     resave: false,
   })
 );
+
+const corsOptions = {
+  origin: "http://localhost:8080",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
